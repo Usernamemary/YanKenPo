0efb84dce3287b6ff860c04eaaaa4353abbc67b0
Usernamemary <47636166+Usernamemary@users.noreply.github.com>
2022-03-25 22:02:31 -0500

    Version switch

--- pegar.js
+++ pegar.js
@@ -26,3 +26,64 @@ function azar() {
 }
 
 azar();
+
+
+//Version switch - Otra version para copiar y pegar-
+
+var opcion = prompt('Escribe piedra, papel o tijera');
+var opcionCompu = Math.floor(Math.random() * 3);
+
+switch(opcionCompu) {
+  case 1:
+    opcionCompu = "papel"    
+    break;
+  
+  case 2:
+    opcionCompu = "tijera"
+    break;
+  case 0:
+    opcionCompu = "piedra"    
+    break;
+
+    
+}
+
+
+
+function azar() {
+
+ switch(true) {
+    case (opcion == "tijera" && opcionCompu == "tijera"):
+    case (opcion == "piedra" && opcionCompu == "piedra"):
+    case (opcion == "papel" && opcionCompu == "papel"):
+      
+    alert (`PC: Yo elegí ${opcionCompu}. Empatamos ╮（￣▽￣）╭`)
+
+    break;
+    case (opcion == "tijera" && opcionCompu == "papel"):
+    case (opcion == "piedra" && opcionCompu == "tijera"):
+    case (opcion == "papel" && opcionCompu == "piedra"):
+  
+    alert (`PC: Yo elegí ${opcionCompu}. Ganaste, ¡hoy es tu día de suerte!  (っ◕‿◕)っ `)
+
+    break;
+
+    case (opcion == "tijera" && opcionCompu == "piedra"):
+    case (opcion == "piedra" && opcionCompu == "papel"):
+    case (opcion == "papel" && opcionCompu == "tijera"):  
+
+    alert (`PC: Yo elegí ${opcionCompu}. La máquina siempre gana ( •̀ᴗ•́ )و ̑̑ `)
+
+    break;
+
+    default: 
+
+    alert (`PC: Porfis escribe piedra, papel o tijera en minúscula y sin espacios adicionales o signos extra.`)
+
+}
+
+
+}
+
+azar();
+
